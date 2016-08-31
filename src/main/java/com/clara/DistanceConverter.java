fd59abaab465f16e8a0cc1786f0bc448d6a62a04
we4954cp <lalala@lsdlfsldf.com>
2016-08-31 18:29:14 -0500

    Meters to feet conversion

---
+++ src/main/java/com/clara/DistanceConverter.java
@@ -0,0 +1,32 @@
+package com.clara;
+
+import java.util.Scanner;
+
+public class DistanceConverter {
+
+    static Scanner numberScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("Please type in a distance in meters:");
+        int meters = numberScanner.nextInt();
+
+        int feet = metersToFeet(meters);
+
+        System.out.println("In feet, that's " + feet + " ft");
+
+        // End of your code here
+        numberScanner.close();
+    }
+
+
+    public static int metersToFeet(int meters){
+        //TODO! This method doesn't work yet. Can you fix it?
+        //To convert meters to feet, multiply by 3
+
+        return 0;
+    }
+
+    // I know doubles would be more real world but we are keeping it simple
+    // int values are a little easier to test - doubles/float are not as precise
+}
---
+++ src/test/java/MetersToFeetTest.java
@@ -0,0 +1,19 @@
+import com.clara.DistanceConverter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MetersToFeetTest {
+
+    @Test
+    public void testMetersToFeet() {
+
+        // Check to see if the expected result is the same
+        // as the actual result from the method.
+        // So we expect metersToFeet with an argument of 10 to return 30
+        assertEquals(0, DistanceConverter.metersToFeet(0));
+        assertEquals(30, DistanceConverter.metersToFeet(10));
+        assertEquals(150, DistanceConverter.metersToFeet(50));
+
+
+    }
+}
